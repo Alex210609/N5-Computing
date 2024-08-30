0c1716063a08ddddcfb2a872d06b46fc27e810ac
Rotund_Potato <99081446+Alex210609@users.noreply.github.com>
2024-08-30 12:58:27 +0100

    Add files via upload

---
+++ Travel agent 1-d array.py
@@ -0,0 +1,19 @@
+# Code using 1d array
+#30.08.2024
+#Alex Blyth
+
+travel_agent = []
+booking_number = []
+dates = []
+
+for index in range(3) :
+    travelagent = input("Enter travel agent")
+    bookingnumber = int(input("Enter booking number"))
+    date = input("Enter date")
+    booking_number.append(bookingnumber)
+    dates.append(date)
+    travel_agent.append(travelagent)
+
+print(travel_agent)
+print(booking_number)
+print(dates)
