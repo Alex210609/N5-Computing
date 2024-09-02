11eb158cf1ec9625ba26fa03a9aa26f439317acd
Rotund_Potato <99081446+Alex210609@users.noreply.github.com>
2024-09-02 12:08:56 +0100

    Update Travel agent 1-d array.py

--- Travel agent 1-d array.py
+++ Travel agent 1-d array.py
@@ -10,9 +10,9 @@ for index in range(3) :
     travelagent = input("Enter travel agent")
     bookingnumber = int(input("Enter booking number"))
     date = input("Enter date")
+    travel_agent.append(travelagent)
     booking_number.append(bookingnumber)
     dates.append(date)
-    travel_agent.append(travelagent)
 
 print(travel_agent)
 print(booking_number)
