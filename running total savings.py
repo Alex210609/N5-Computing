c71a34565f16b7266b900bd8dbebf7da43f4b67e
Rotund_Potato <99081446+Alex210609@users.noreply.github.com>
2024-10-02 09:04:47 +0100

    Add files via upload

---
+++ running total savings.py
@@ -0,0 +1,5 @@
+total = 0
+for i in range (12):
+    total = total + int(input("How much u savin this month"))
+    print("U have £",total," saved so far this year")
+print("U saved £",total, "this year")
---
+++ running total test scores.py
@@ -0,0 +1,6 @@
+total = 0
+
+for i in range(5):
+    grades = int(input("What did they get on test"))
+    total = total + grades
+    print ("the scores add up to",total)
