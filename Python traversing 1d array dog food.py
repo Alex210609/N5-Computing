77369b67a449262e45f78b69757016b5e08feef7
Rotund_Potato <99081446+Alex210609@users.noreply.github.com>
2024-10-04 12:44:52 +0100

    Add files via upload

---
+++ Python traversing 1d array dog food.py
@@ -0,0 +1,7 @@
+weights = [2,2,2,2,2,2,2]
+total_weight = 0
+
+for index in range (7):
+    total_weight = total_weight+weights[index]
+    
+print(total_weight)
---
+++ Python traversing 1d array temps.py
@@ -0,0 +1,8 @@
+temps = [2,2,2,2,2,2,2]
+total_temp = 0
+
+for index in range (7):
+    total_temp = total_temp+temps[index]
+
+avg_temp = total_temp/7
+print(avg_temp)
